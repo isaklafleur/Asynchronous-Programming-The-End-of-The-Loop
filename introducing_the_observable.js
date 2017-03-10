0a03114cfaab1cd01ee6b7060d7f61ab32345ef4
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-10 13:08:33 +0100

    .

--- introducing_the_observable.js
+++ introducing_the_observable.js
@@ -1,7 +1,56 @@
 // https://egghead.io/lessons/javascript-introducing-the-observable
 // https://github.com/ReactiveX/rxjs
-var Rx = require('rxjs/Rx');
+
+// Asynchronous Programming: The End of The Loop
+
+//var Rx = require('rxjs/Rx');
 
 var Observable = Rx.Observable;
 
-console.log(Observable.of(1,2,3));
+var button = document.getElementById('button');
+
+// Classic way...
+/*
+var handler = function(e) {
+    alert('clicked');
+    button.removeEventListener('click', handler);
+};
+
+button.addEventListener('click', handler);
+*/
+
+///////////////////////
+// https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.0.1/Rx.js
+// https://cdnjs.cloudflare.com/ajax/libs/rxjs/4.1.0/rx.all.js
+// Working with Asynchronous with Obserables 4.1
+
+var clicks = Observable.fromEvent(button, 'click');
+
+    var subscription =
+    clicks.forEach(
+        function onNext(e) {
+            alert('clicked');
+            subscription.dispose(); // remove the "EventListener"
+        },
+        function onError(error) {
+            console.log('ERROR!');
+        },
+        function onCompleted() {
+            console.log('done');
+        });
+
+//////////////////////
+
+// Working with Synchronous 
+/*
+try {
+    [1,2,3].forEach(function(i) {
+    console.log(i);
+});
+console.log('done');
+}
+catch(e) {
+    console.log('ERROR!');
+}
+*/
+////////////////////
---
+++ using_map_method_with_observable.js
@@ -0,0 +1,24 @@
+
+var Observable = Rx.Observable;
+var button = document.getElementById('button');
+
+
+var clicks = Observable.fromEvent(button, 'click');
+
+var points =
+    clicks.map(function(e) {
+        return{x: e.clientX, y: e.clientY};
+    });
+
+    var subscription =
+    points.forEach(
+        function onNext(point) {
+            alert('clicked' + JSON.stringify(point));
+            subscription.dispose(); // remove the "EventListener"
+        },
+        function onError(error) {
+            console.log('ERROR!');
+        },
+        function onCompleted() {
+            console.log('done');
+        });
